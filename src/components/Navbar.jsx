5d8dea2c020771c5841f910013b3a29ca7a78719
DeadBread-001 <nikita10062004m@gmail.com>
2025-04-19 13:34:47 +0300

    Исправление импорта

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useLocation } from 'react-router-dom';
+import { Link } from 'react-router'
 
 // Временная функция для проверки прав доступа
 const isAdmin = () => {
