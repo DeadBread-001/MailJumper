12521b94d91bb0395c61da3eab49eb09d2567bc6
DeadBread-001 <nikita10062004m@gmail.com>
2025-06-25 12:19:17 +0300

    Поправил ручку выполнения задания

--- src/api/tasks.js
+++ src/api/tasks.js
@@ -23,7 +23,7 @@ export async function getTasks() {
  * @throws {Error} Если произошла ошибка или некорректный формат данных
  */
 export async function completeTask(token, vkid) {
-    const url = IP + `shop/${vkid}/task`;
+    const url = IP + `admin/${vkid}/task`;
     const data = await fetchRequest(url, 'POST', token);
     if (data.Status === 200) {
         return data;
